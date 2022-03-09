69c33f825fc7a7917f6024e7b6053d91813f2714
Kitschminyof <stephan.kitschminyof@etu.univ-savoie.fr>
2022-03-09 10:59:57 +0100

    condition empty input

--- AppSplashConfiguration/static/script.js
+++ AppSplashConfiguration/static/script.js
@@ -4,18 +4,30 @@ var parcels = []
 
 function addParcel() {
     // Get values from html form
-    let name = document.getElementById("name").value;
-    let plant = document.getElementById("plant").value;
-    let dim = document.getElementById("dimension").value;
+    let name = document.getElementById("name");
+    let plant = document.getElementById("plant");
+    let dim = document.getElementById("dimension");
 
-    // Init parcel and add parcel
-    let parcel = {
-        name: name,
-        plant: plant,
-        dim: dim
+    // Throw error if empty input
+    if (name.value == "" || plant.value == "" || dim.value == "") {
+        throw 'Invalid form values !';
     }
 
+    // Init new parcel
+    let parcel = {
+        name: name.value,
+        plant: plant.value,
+        dim: dim.value,
+    }
+
+    // Add parcel to parcels list
     parcels.push(parcel)
+
+    // Reset form 
+    name.value = "";
+    plant.value = "";
+    dim.value = "";
+
 
     console.log("Parcelles : ", parcels)
 }
