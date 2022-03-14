16045a1587e80361c9ce639f11aad2a9f485a0a5
Kitschminyof <stephan.kitschminyof@etu.univ-savoie.fr>
2022-03-14 09:36:40 +0100

    init algo splash

---
+++ main.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+
+def main():
+    # Attendre que data.json existe
+
+    algoSplash()
+
+
+def algoSplash():
+    # Afficher tout va bien
+    sleep(5)
+    
+    # Afficher qu'il faut arroser
