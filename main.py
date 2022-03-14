0a4b7f937e4e437a2bc573715fb9c15a3db666a8
Benjamin Gallet <benjamin.gallet@api-k.com>
2022-03-14 13:04:01 +0100

    Amélioration de l'affichage de la météo

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from time import sleep
 from meteofrance_api import MeteoFranceClient
-#from LCDScreen import LCDScreen
+from LCDScreen import LCDScreen
 import json
 
 meteo = MeteoFranceClient()
 
-#screen = LCDScreen()
+screen = LCDScreen()
 
 jsonData = None
 jsonConfig = None
@@ -27,7 +27,7 @@ def algoSplash():
     jsonConfig = json.load(file)
 
     # Afficher tout va bien
-    #sleep(5)
+    sleep(5)
     
     # Afficher qu'il faut arroser
 
@@ -41,16 +41,29 @@ def afficheNiveauEau():
     global capaciteCuve
 
     water_level = jsonData["water_level"]
-    #screen.setText("Niveau d'eau :"+str(water_level))    
-    print("Niveau d'eau : "+str(water_level/(capaciteCuve/1024))+"L sur "+str(capaciteCuve)+"L")
+    screen.setText("Niveau d'eau :  "+str(round(water_level*(capaciteCuve/1024),3))+"L/"+str(capaciteCuve)+"L")    
+    sleep(2)
+    screen.setText("")
+    
+    print("Niveau d'eau :  "+str((water_level*(capaciteCuve/1024)))+"L/"+str(capaciteCuve)+"L")
 
 def afficheMeteo():
     global meteo
+    
     previsions = meteo.get_forecast(45.64135619912203, 5.870128080576113,"fr").daily_forecast[1]
     temp = (previsions["T"]["min"]+previsions["T"]["max"])/2
     humidite = (previsions["humidity"]["min"]+previsions["humidity"]["max"])/2
     precipitation = previsions["precipitation"]["24h"]
-    #screen.setText("Météo  Température : "+str(temp)+"°C   -  Humidité : "+str(humidite)+"%   -  Précipitations (24H) : "+str(precipitation)+"%")
+    
+    #32 car max par setText(txt)
+    screen.setText("Meteo :         Temp : "+str(round(temp,1))+"-C")
+    sleep(2)
+    screen.setText("Meteo :         Humidite : "+str(round(humidite,1))+"%")
+    sleep(2)
+    screen.setText("Meteo :         Pluie : "+str(round(precipitation,1))+"%")
+    sleep(2)
+    screen.setText("")
+    
     print("Météo  Température : "+str(temp)+"°C   -  Humidité : "+str(humidite)+"%   -  Précipitations (24H) : "+str(precipitation)+"%")
 
 algoSplash()
