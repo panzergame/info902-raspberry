e0bff81062cc0ad95043e9902b446a46df8644f7
Kitschminyof <stephan.kitschminyof@etu.univ-savoie.fr>
2022-03-14 10:36:15 +0100

    check if file exist

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from time import sleep
 
+def checkFileExist(filePath):
+    """Vérifier si un fichier existe.
+
+    Parameters
+    ----------
+    filepath : string 
+        le path du fichier dont on veut vérifier l'existance.
+
+    Returns
+    -------
+    boolean
+        True si le fichier exist, False si le fichier n'existe pas.
+
+    """
+    
+    try:
+        with open(filePath, 'r') as f:
+            return True
+    except FileNotFoundError as e:
+        return False
+    except IOError as e:
+        return False
 
 def main():
     # Attendre que data.json existe
