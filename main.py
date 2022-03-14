5039c6e80c45eca086e6e7412d7594ecf6ef75d8
Kitschminyof <stephan.kitschminyof@etu.univ-savoie.fr>
2022-03-14 11:19:00 +0100

    init watering function

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import json
 from time import sleep
+from meteofrance_api import MeteoFranceClient
+
+
+splashTaskState = "Nothing" # Définit l'état de Splash, Nothing : rien à faire / Todo : une tâche d'arrosage à faire / During : en cours de réalisation
 
 def checkFileExist(filePath):
     """Vérifier si un fichier existe.
@@ -38,5 +43,25 @@ def algoSplash():
     sleep(5)
     
     # Afficher qu'il faut arroser
+    splashTaskState = "Todo"
+
+def watering():
+    if splashTaskState == "Todo" :
+        meteo = MeteoFranceClient()
+
+        prevision = meteo.get_forecast(45.527532, 5.960843,"fr")
+        print(prevision.daily_forecast)
+
+    # Opening data.json file
+    f = open('data.json')
+    data = json.load(f)
+ 
+    # Itération sur data
+    for i in data['emp_details']:
+        print(i)
+ 
+    # Closing file
+    f.close()
+
 
 main()
